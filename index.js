054a8da865563fbe04e9660e0833585502ea1e99
Vitor Vicente <vi_dualcore@hotmail.com>
2019-12-11 16:37:36 -0300

    .

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const database  = require('./src/database/config');
 const requireDir = require('require-dir');
+const cors = require('cors');
 const app = express();
 
 requireDir('./src/models');
 
 app.use(express.json());
 app.use('/api',require('./src/routes'))
+app.use(cors());
 
 app.listen(3001, function(){
        console.log('Rodando na porta 3001');
